4c10654ca3990034e9d136f225fb791935e21d26
ZulfaSalsabila <zulfa130802@gmail.com>
2022-11-14 21:31:23 +0700

    first projects KTM

---
+++ src/main/java/pws/c/dataKtm/DataKtmApplication.java
@@ -0,0 +1,13 @@
+package pws.c.dataKtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataKtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataKtmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pws/c/dataKtm/DataKtmApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.c.dataKtm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataKtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
